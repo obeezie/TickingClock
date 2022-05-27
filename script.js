4b0611145d3eb6c42aae3aa1075daba9629834bd
Martin Vera <m.vera1996@gmail.com>
2022-05-27 12:18:52 -0700

    working clock

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ var currentSecond = new Date().getSeconds();
 var currentMinutes = new Date().getMinutes();
 var currentHours = new Date().getHours();
 
-console.log(currentSecond, currentMinutes, currentHours);
-
-// function getSecondsSinceStartOfDay() {
-//   return (
-//     new Date().getSeconds() +
-//     new Date().getMinutes() * 60 +
-//     new Date().getHours() * 3600
-//   );
-// }
-
-console.log(new Date().getMinutes());
+//Sets starting time
+var seconds = document.getElementById("seconds");
+var hour = document.getElementById("hour");
+var minutes = document.getElementById("minutes");
+var secondAngle = (currentSecond - 30) * 6;
+var hourAngle = (12 - 30) * 30;
+var minuteAngle = (currentMinutes - 30) * 6;
+seconds.style.setProperty("transform", `rotate(${secondAngle}deg)`);
+hour.style.setProperty("transform", `rotate(${hourAngle}deg)`);
+minutes.style.setProperty("transform", `rotate(${minuteAngle}deg)`);
 
 setInterval(function () {
   // var time = getSecondsSinceStartOfDay();
   var currentSecond = new Date().getSeconds();
   var currentMinutes = new Date().getMinutes();
   var currentHours = new Date().getHours();
-  var secondAngle = currentSecond * 360;
+  var secondAngle = (currentSecond - 30) * 6;
+  var hourAngle = (currentHours - 30) * 30;
+  var minuteAngle = (currentMinutes - 30) * 6;
   // console.log(currentSecond * 360);
   var seconds = document.getElementById("seconds");
   var hour = document.getElementById("hour");
   var minutes = document.getElementById("minutes");
+  seconds.style.setProperty("transform", `rotate(${secondAngle}deg)`);
+  hour.style.setProperty("transform", `rotate(${hourAngle}deg)`);
+  minutes.style.setProperty("transform", `rotate(${minuteAngle}deg)`);
 }, 1000);
